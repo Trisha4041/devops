6279bd34d82acf4e064c72be569c06aab3f25e72
Trisha4041 <146335284+Trisha4041@users.noreply.github.com>
2026-01-29 12:04:59 +0530

    Project created

---
+++ YCCECalc/Class1.cs
@@ -0,0 +1,18 @@
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace YCCECalc;
+
+public class MyMaths
+{
+    public int add2nos(int a, int b)
+    {
+        if(a>0 && b>0)
+        {
+            return a + b;
+        }
+        else 
+        {
+            return 0;
+        }
+    }
+}
